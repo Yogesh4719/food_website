958ac590399cee710535cc4e9e77e27ced94c748
Yogesh Kumar Singh <154615269+Yogesh4719@users.noreply.github.com>
2025-08-30 22:10:01 +0530

    Add files via upload

---
+++ src/components/LoginPopup/LoginPopup.jsx
@@ -0,0 +1,33 @@
+import React,{useState} from 'react'
+import './LoginPopup.css'
+import {assets} from '../../assets/assets'
+const LoginPopup = ({setShowLogin}) => {
+    const[currState,setCurrState]=useState("Sign Up");
+  return (
+    <div className='login-popup'>
+        <form  className="login-popup-container">
+            <div className="login-popup-title">
+                <h2>{currState}</h2>
+                <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
+            </div>
+            <div className="login-popup-inputs">
+                {currState==="Login"?<></>:
+                <input type="text" placeholder='Your Name' required />}
+                <input type="email" placeholder='Your Email' required />
+                <input type="password" placeholder='Your Password' required />
+            </div>
+            <button>{currState==="Sign Up"?"Create Account":"Login"}</button>
+            <div className="login-popup-condition">
+                <input type="checkbox" required />
+                <p>I agree to the Terms of Service and Privacy Policy</p>
+            </div>
+            {currState==="Login"?
+            <p onClick={()=>setCurrState("Sign Up")}>Create a New Account?<span>Click Here</span></p>:<p onClick={()=>setCurrState("Login")}>Already have an account?<span>Login Here</span></p>
+            
+}
+        </form>
+    </div>
+  )
+}
+
+export default LoginPopup
---
+++ src/context/StoreContext.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import{ createContext } from "react";
+import { food_list } from "../assets/assets";
+export const StoreContext= createContext(null);
+function StoreContextProvider(props) {
+    const [cartItems,setCartItems]=useState({});
+
+    const addToCart=(itemId)=>{
+        if(!cartItems[itemId]){
+            setCartItems((prev)=>({...prev,[itemId]:1}));
+        }
+        else{
+            setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}));
+        }
+    }
+    const removeFromCart=(itemId)=>{
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}) );
+    }
+    const getTotalCartAmount=()=>{
+        let totalAmount=0;
+        for(const item in cartItems){
+            if(cartItems[item]>0){
+                let itemInfo=food_list.find((food)=>food._id===item);
+                totalAmount+=cartItems[item]*itemInfo.price;
+            }
+        }
+        return totalAmount;
+    }
+    
+    const contextValue = {
+        food_list,
+        cartItems,
+        setCartItems,
+        addToCart,
+        removeFromCart,
+        getTotalCartAmount
+    }
+    return (
+        <StoreContext.Provider value={contextValue}>
+            {props.children}
+        </StoreContext.Provider>
+    );
+}
+export default StoreContextProvider;
